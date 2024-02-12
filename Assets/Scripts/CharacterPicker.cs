c038f6cb0de1c3fa3f4864f86cbd8bb027fa7f05
Maxirus98 <dupuismaxime@hotmail.com>
2024-02-12 13:12:53 -0500

    Added temp CharacterPickRpc

--- Assets/Scripts/CharacterPicker.cs
+++ Assets/Scripts/CharacterPicker.cs
@@ -1,42 +1,18 @@
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class CharacterPicker : NetworkBehaviour
 {
-    [SerializeField]
-    private List<Transform> models = new List<Transform>();
-    private ushort modelIndex;
-
-    public override void OnNetworkSpawn()
+    [Rpc(SendTo.ClientsAndHost)]
+    public void AssignPlayerModelRpc()
     {
-        modelIndex = (ushort) (IsHost ? 0 : 1);
-        if (!IsOwner)
-        {
-            Destroy(this);
-        }
+        Debug.Log($"Called Assigned Player model from {NetworkManager.LocalClientId}");
+        var modelIndex = NetworkManager.LocalClientId == 0 ? 0 : 1;
+        var playerPrefabTransform = NetworkManager.Singleton.LocalClient.PlayerObject.transform;
+        var newPosition = playerPrefabTransform.position;
+        newPosition.x = modelIndex == 0 ? -4 : 4;
 
-        AssignPlayerModel();
-    }
-
-    [Rpc(SendTo.Server)]
-    public void SwitchCharacterRpc()
-    {
-        // var localClientId = NetworkManager.Singleton.LocalClientId;
-        modelIndex = (ushort) (modelIndex == 0 ? 0 : 1);
-
-        // Cherche le ClientObject
-        // if (!NetworkManager.Singleton.ConnectedClients.TryGetValue(localClientId, out var client)) return;
-
-        if(IsOwner)
-        {
-            AssignPlayerModel();
-        }
-    }
-
-    private void AssignPlayerModel()
-    {
-        //var playerPrefabTransform = NetworkManager.Singleton.LocalClient.PlayerObject.transform;
-        //models[modelIndex].parent = playerPrefabTransform;
+        playerPrefabTransform.position = newPosition;
+        playerPrefabTransform.GetChild((int) modelIndex).gameObject.SetActive(true);
     }
 }
