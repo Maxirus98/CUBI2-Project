576071ac4b1f59befe9fbd0424700e2687837e8e
Maxirus98 <dupuismaxime@hotmail.com>
2024-02-14 17:35:30 -0500

    Enable PlayerGameObject on start ui destroy

--- Assets/StartGameUi.cs
+++ Assets/StartGameUi.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StartGameUi : MonoBehaviour
@@ -13,20 +16,27 @@ public class StartGameUi : MonoBehaviour
 
         if (!NetworkManager.Singleton.IsHost)
         {
-            startButton.gameObject.SetActive(false);
+            startButton.interactable = false;
         }
     }
 
     private void OnClickStart()
     {
         NetworkLoader.LoadNetwork(NetworkLoader.Scene.LevelMaxime);
-        EnablePlayerControllerRpc();
         RelayConnectionManager.Instance.DisablePasswordUi();
     }
 
-    private void EnablePlayerControllerRpc()
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void EnablePlayerControllerRpc(string sceneName, LoadSceneMode loadSceneMode, List<ulong> clientsCompleted, List<ulong> clientsTimedOut)
     {
-        //var playerPrefab = NetworkManager.Singleton.LocalClient.PlayerObject;
+        var playerPrefab = NetworkManager.Singleton.LocalClient.PlayerObject;
+        var connectedClients = NetworkManager.Singleton.ConnectedClientsIds;
+        foreach (var item in connectedClients)
+        {
+            Debug.Log($"Connected client ids: {item}");
+        }
+
         Debug.Log($"Player with id {NetworkManager.Singleton.LocalClient} can now move");
         var players = GameObject.FindGameObjectsWithTag("Player");
         foreach (var player in players)
@@ -36,5 +46,10 @@ public class StartGameUi : MonoBehaviour
             playerController.enabled = true;
             rb.useGravity = true;
         }
+    }
+
+    private void OnDestroy()
+    {
+        NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += EnablePlayerControllerRpc;
     }
 }
