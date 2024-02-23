d26626f5baa89547f8daea0ebba53c49bfe59e55
nexy-us <mathilde.c.pc@gmail.com>
2024-02-22 23:02:36 -0500

    Spawn ennemi + se dirigent vers un point fixe

--- Assets/ControlPlayer.cs
+++ Assets/ControlPlayer.cs
@@ -1,24 +1,18 @@
 
+using System;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Apple;
 
-public class ControlPlayer : MonoBehaviour
-{
-    // Script 'test' pour déplacer l'ia avec un clic sur le terrain
-    public Camera cam;
+public class ControlPlayer : MonoBehaviour {
+    // Deplacer les ennemis vers un point fixe
 
     public NavMeshAgent agent;
+    public Vector3 destination = new Vector3(19f, 0.89f, 19f); // Destination finale
 
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0)) {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+    void Update() {
 
-            if(Physics.Raycast(ray, out hit)) {
-                agent.SetDestination(hit.point);
-            }
-
-        }
+        agent.SetDestination(destination);
+        
     }
 }
