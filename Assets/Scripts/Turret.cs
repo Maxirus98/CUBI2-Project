4ea3e8d07c17200330d123e62c8562d8aa687cb2
Miliya-Ai <102935805+miliya-ai@users.noreply.github.com>
2024-02-24 16:38:57 -0500

    cannon build avec keyE

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-    public List<GameObject> ennemies = new List<GameObject>();
+    public GameObject cannonPrefab;
+    public bool isPlayerInRange = false;
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            BuildCannon();
+        }
+    }
+
     void OnTriggerEnter(Collider col)
     {
-        if(col.tag == "Enemy")
+        if (col.tag == "Enemy")
         {
-            ennemies.Add(col.gameObject);
+            enemies.Add(col.gameObject);
+        }
+        else if (col.tag == "Player")
+        {
+            isPlayerInRange = true;
         }
     }
 
     void OnTriggerExit(Collider col)
     {
-        if(col.tag == "Enemy")
+        if (col.tag == "Enemy")
         {
-            ennemies.Remove(col.gameObject);
+            enemies.Remove(col.gameObject);
+        }
+        else if (col.tag == "Player")
+        {
+            isPlayerInRange = false;
+        }
+    }
+
+    void BuildCannon()
+    {
+        if (cannonPrefab != null)
+        {
+            Vector3 buildPosition = transform.position + new Vector3(0, 4f, 0);
+            GameObject canonInstance = Instantiate(cannonPrefab, buildPosition, Quaternion.Euler(0, 0, 90));
         }
     }
 }
