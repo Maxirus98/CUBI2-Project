73660be775382a8b72c7618df399e298a8e9d116
Maxirus98 <dupuismaxime@hotmail.com>
2024-03-01 12:35:51 -0500

    Not DONE, Added script to gain and spend useable resource like water and sand

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -3,17 +3,23 @@
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField]
-    private HealthBar healthBar;
-    private HealthBar healthBarClone;
+    private PlayerResourceScript playerResource;
+    private PlayerResourceScript playerResourceClone;
 
     [SerializeField]
     private int maxHealth = 5;
     private int currentHealth;
 
+    private int maxUseable = 5;
+    private int currentUseable;
+
+    private float resourceDetectionRadius = 3f;
+
     void Start()
     {
-        InitializeHealthbar();
+        InitializePlayerResource();
     }
+
 
     public void OnCollisionEnter(Collision collision)
     {
@@ -33,11 +39,29 @@ public class PlayerStats : MonoBehaviour
         }
     }
 
-    private void InitializeHealthbar()
+    /// <summary>
+    /// Action d'ajouter des ressources comme l'eau ou le sable lorsqu'on intéragit près de ces ressources.
+    /// </summary>
+    public void AddNearbyResource(string resourceTag)
     {
-        healthBarClone = Instantiate(healthBar, Vector3.zero, Quaternion.identity);
+        var playerPosition = transform.position;
+        if (Physics.SphereCast(playerPosition, resourceDetectionRadius, transform.position, out RaycastHit hit))
+        {
+            if (hit.collider.CompareTag(resourceTag))
+            {
+                // TODO: Add resource to player pool
+            }
+        }
+    }
+
+    private void InitializePlayerResource()
+    {
+        playerResourceClone = Instantiate(playerResource, Vector3.zero, Quaternion.identity);
         currentHealth = maxHealth;
-        healthBarClone.SetMaxHealth(maxHealth);
+        playerResourceClone.SetMaxHealth(maxHealth);
+
+        currentUseable = maxUseable;
+        playerResourceClone.SetMaxUseable(maxUseable);
     }
 
     private void TakeDamage(int damage)
@@ -45,7 +69,27 @@ public class PlayerStats : MonoBehaviour
         if(currentHealth > 0)
         {
             currentHealth -= damage;
-            healthBarClone.SetHealth(currentHealth);
+            playerResourceClone.SetHealth(currentHealth);
         }
+    }
+
+    public void UseResource(int spending)
+    {
+        currentUseable -= spending;
+        playerResourceClone.SetUseable(currentUseable);
+    }
+
+    private void GainResource(int gain)
+    {
+        if(currentUseable < maxUseable)
+        {
+            currentUseable += gain;
+            playerResourceClone.SetUseable(currentUseable);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, resourceDetectionRadius);
     }
 }
---
+++ Assets/Scripts/PlayerResourceScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script attaché à un élément healthbar pour le contrôler
+/// </summary>
+public class PlayerResourceScript : MonoBehaviour
+{
+    [SerializeField]
+    private Slider healthSlider;
+    [SerializeField]
+    private Gradient healthGradient;
+    [SerializeField]
+    private Image fill;
+
+    [SerializeField]
+    private Slider useableSlider;
+
+    public void SetMaxHealth(int health)
+    {
+        Debug.Log("Setting max health to " + health);
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+
+        fill.color = healthGradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int health)
+    {
+        Debug.Log("Setting health to " + health);
+        healthSlider.value = health;
+        fill.color = healthGradient.Evaluate(healthSlider.normalizedValue);
+    }
+
+    public void SetMaxUseable(int resource)
+    {
+        Debug.Log("Setting max useable to " + resource);
+        useableSlider.maxValue = resource;
+        useableSlider.value = resource;
+    }
+
+    public void SetUseable(int resource)
+    {
+        Debug.Log("Setting useable to " + resource);
+        useableSlider.value = resource;
+    }
+}
