640f3e7ce266892cf4553e78b2c8b2182d83a615
Maxirus98 <dupuismaxime@hotmail.com>
2024-03-06 15:26:27 -0500

    Added win lose handler script to game manager

--- Assets/Scripts/WinLoseHandler.cs
+++ Assets/Scripts/WinLoseHandler.cs
@@ -1,35 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 
 /// <summary>
 /// Script permettant de déterminer si les joueurs ont gagnés ou perdus
 /// </summary>
-public class WinLoseHandler : MonoBehaviour
+public class WinLoseHandler : Singleton<WinLoseHandler>
 {
-    [System.Serializable] public class EventGameState : UnityEvent<GameState, GameState> { }
     public enum GameState
     {
         Running,
         Won,
         Lost
     }
-    public EventGameState gameStateHandler;
-    public GameState CurrentGameState { get; set; }
 
-    void UpdateGameState(GameState newGameState)
+    [SerializeField]
+    private GameObject endGameCanvas;
+    [SerializeField]
+    private TextMeshProUGUI endGameText;
+    [SerializeField]
+    private AudioSource gameAudioSource;
+    [SerializeField]
+    private AudioClip gameLostAudio;
+    [SerializeField]
+    private AudioClip gameWonAudio;
+
+    public GameState CurrentGameState {get; set;}
+
+    public void UpdateGameState(GameState newGameState)
     {
-        var previousGameState = CurrentGameState;
         CurrentGameState = newGameState;
         switch (CurrentGameState)
         {
             case GameState.Running:
+                
+                break;
+            case GameState.Won:
+                EndGame("Partie gagnée!", gameWonAudio);
+                break;
+            case GameState.Lost:
+                EndGame("Partie perdue", gameLostAudio);
                 break;
             default:
                 break;
         }
 
-        gameStateHandler.Invoke(CurrentGameState, previousGameState);
+        Time.timeScale = CurrentGameState == GameState.Running ? 1 : 0;
+    }
+
+    private void EndGame(string text, AudioClip oneShotClip)
+    {
+        endGameText.text = text;
+        //gameAudioSource.PlayOneShot(oneShotClip);
+        endGameCanvas.SetActive(true);
     }
 }
