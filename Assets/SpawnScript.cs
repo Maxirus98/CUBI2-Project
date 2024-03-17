8209525f8fa33ee1aa691683e5a8640a5a65a391
nexy-us <mathilde.c.pc@gmail.com>
2024-03-17 18:30:57 -0400

    waves + timer (en cours)

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -1,44 +1,84 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class SpawnScript : MonoBehaviour
-{
+public class SpawnScript : MonoBehaviour {
     public bool isTesting = false;
 
     [SerializeField]
-    public GameObject ennemy;
+    public GameObject enemy1;
+    public GameObject enemy2;
 
     [SerializeField]
     Transform[] spawnPoint;
 
-    [SerializeField]
     public int NumberOfEnemies1;
     public int NumberOfEnemies2;
 
-    void Start()
-    {
-        if (!isTesting && !NetworkManager.Singleton.IsHost) return;
+    [SerializeField]
+    public int Wave1Enemies1;
+    public int Wave2Enemies1;
+    public int Wave2Enemies2;
+    public int Wave3Enemies1;
+    public int Wave3Enemies2;
+
+    public int numWave = 1;
+    public int totalEnemies;
+
+    private TimerScript timerScript;
+
+    void Start() {
+
+        timerScript = GetComponent<TimerScript>();
+
+        NumberOfEnemies1 = Wave1Enemies1;
+        if (!isTesting && !NetworkManager.Singleton.IsHost)
+            return;
         spawnPoint = new Transform[transform.childCount];
 
-        for (int i=0; i<transform.childCount; i++) {
+        for (int i = 0; i < transform.childCount; i++) {
             spawnPoint[i] = transform.GetChild(i);
+        }
+        if (numWave == 1) {
+            NumberOfEnemies1 = Wave1Enemies1;
+            NumberOfEnemies2 = 0;
+            totalEnemies = NumberOfEnemies1;
+        }
+        else if (numWave == 2) {
+            NumberOfEnemies1 = Wave2Enemies1;
+            NumberOfEnemies2 = Wave2Enemies2;
+            totalEnemies = NumberOfEnemies1 + NumberOfEnemies2;
+        }
+        else if (numWave == 3) {
+            NumberOfEnemies1 = Wave3Enemies1;
+            NumberOfEnemies2 = Wave3Enemies2;
+            totalEnemies = NumberOfEnemies1 + NumberOfEnemies2;
         }
 
         for (int i = 0; i < NumberOfEnemies1; i++) {
 
             Transform randomPoint = spawnPoint[Random.Range(0, spawnPoint.Length)];
 
-            var enemyInstance = Instantiate(ennemy, randomPoint.transform);
-            var instanceNetworkObject = enemyInstance.GetComponent<NetworkObject>(); 
+            var enemyInstance = Instantiate(enemy1, randomPoint.transform);
+            var instanceNetworkObject = enemyInstance.GetComponent<NetworkObject>();
+            instanceNetworkObject.Spawn();
+        }
+
+        for (int i = 0; i < NumberOfEnemies2; i++) {
+
+            Transform randomPoint = spawnPoint[Random.Range(0, spawnPoint.Length)];
+
+            var enemyInstance = Instantiate(enemy2, randomPoint.transform);
+            var instanceNetworkObject = enemyInstance.GetComponent<NetworkObject>();
             instanceNetworkObject.Spawn();
         }
     }
 
-    private void Update()
-    {
-        if(NumberOfEnemies1 <= 0)
-        {
-            WinLoseHandler.Instance.UpdateGameState(GameState.Won);
+    private void Update() {
+        if (totalEnemies <= 0) {
+            numWave++;
+            timerScript.enabled = true;
+
+            //WinLoseHandler.Instance.UpdateGameState(GameState.Won);
         }
     }
 }
