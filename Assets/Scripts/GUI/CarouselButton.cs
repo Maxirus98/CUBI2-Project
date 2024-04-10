f4a74ce8825a8557aa089987359db72aa4171be4
Kelvin Chen <kkcsike@gmail.com>
2024-04-10 01:18:01 -0400

    tutorials

--- Assets/Scripts/GUI/CarouselButton.cs
+++ Assets/Scripts/GUI/CarouselButton.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class CarouselButton : MonoBehaviour
 {
     public RectTransform carouselContainer;
+    public TMP_Text counter;
     public Image[] images;
+    public Button leftButton;
+    public Button rightButton;
 
     private int currentIndex = 0;
 
@@ -16,7 +20,9 @@ public class CarouselButton : MonoBehaviour
         {
             images[i].gameObject.SetActive(false);
         }
-  
+        updateCounter();
+        leftButton.onClick.AddListener(ShiftLeft);
+        rightButton.onClick.AddListener(ShiftRight);
     }
 
     public void ShiftLeft()
@@ -24,6 +30,7 @@ public class CarouselButton : MonoBehaviour
         images[currentIndex].gameObject.SetActive(false);
         currentIndex = (currentIndex - 1 + images.Length) % images.Length;
         images[currentIndex].gameObject.SetActive(true);
+        updateCounter();
     }
 
     public void ShiftRight()
@@ -31,5 +38,11 @@ public class CarouselButton : MonoBehaviour
         images[currentIndex].gameObject.SetActive(false);
         currentIndex = (currentIndex + 1) % images.Length;
         images[currentIndex].gameObject.SetActive(true);
+        updateCounter();
+    }
+
+    private void updateCounter()
+    {
+        counter.text = (currentIndex + 1).ToString() + "/" + (images.Length).ToString();
     }
 }
