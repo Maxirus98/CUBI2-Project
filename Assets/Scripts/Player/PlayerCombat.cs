906bd446ae98f869c169ef2bca36051ea5320523
Maxirus98 <dupuismaxime@hotmail.com>
2024-04-15 01:21:24 -0400

    Fixed bug where client would see the shoot projectile in the direction of their own camera and not the actual projectiles

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -44,39 +44,37 @@ public class PlayerCombat : NetworkBehaviour
 
     public void Attack()
     {
-        var dir = transform.forward;
+        var dir = currentCamera.transform.forward;
+        dir.y = 0;
         // Envoi la requête pour exécuter l'attaque sur tous les clients à partir du même personnage.
         // On évite de spawner un objet sur le network, on fait simplement dire à l'autre joueur de tirer
-        RequestFireServerRpc();
+        RequestFireServerRpc(dir);
 
         // On tire localement
         Debug.Log("Fired locally");
-        ExecuteShoot();
+        ExecuteShoot(dir);
 
         // Anim
         playerAnimatorHandler.PlayTargetAnimationByName("Attack");
     }
 
     [ServerRpc(RequireOwnership = false)]
-    private void RequestFireServerRpc()
+    private void RequestFireServerRpc(Vector3 dir)
     {
         Debug.Log("Fired Server Rpc");
-        FireClientRpc();
+        FireClientRpc(dir);
     }
 
     [ClientRpc]
-    private void FireClientRpc()
+    private void FireClientRpc(Vector3 dir)
     {
         if (!IsOwner) {
-            Debug.Log("Fired client rpc");
-            ExecuteShoot();
+            ExecuteShoot(dir);
         }
     }
 
-    private void ExecuteShoot()
+    private void ExecuteShoot(Vector3 dir)
     {
-        var dir = currentCamera.transform.forward;
-        dir.y = 0;
         transform.rotation = Quaternion.LookRotation(dir);
         var projectile = Instantiate(currentProjectile, currentShootPoint.position, Quaternion.identity);
         projectile.Init(dir * projectileSpeed);
