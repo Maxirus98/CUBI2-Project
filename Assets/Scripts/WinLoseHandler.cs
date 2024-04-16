c69e152833f4da6b2b341377194e2019a2e102cc
Maxirus98 <dupuismaxime@hotmail.com>
2024-04-15 21:49:29 -0400

    Change win and lose text

--- Assets/Scripts/WinLoseHandler.cs
+++ Assets/Scripts/WinLoseHandler.cs
@@ -30,15 +30,15 @@ public class WinLoseHandler : Singleton<WinLoseHandler>
                 break;
             case GameState.DoorLost:
                 loseCanvas.SetActive(true);
-                EndGame("Attention! Les cauchemars ont r�ussi leur objectif. La petite Rosalie se r�veille de son cauchemar.", SoundManager.Instance.lostSound);
+                EndGame("Partie perdue! Les cauchemars ont atteint la porte et réveillé Sophie-Rose.", SoundManager.Instance.lostSound);
                 break;
             case GameState.Won:
                 winCanvas.SetActive(true);
-                EndGame("Bravo! Le r�ve a �t� stabilis�, vous avez vaincu tous les cauchemars", SoundManager.Instance.winSound);
+                EndGame("Partie Gagnée! Vous avez éliminé tous les cauchemars", SoundManager.Instance.winSound);
                 break;
             case GameState.HpLost:
                 loseCanvas.SetActive(true);
-                EndGame("Attention! Un des d�fenseurs oniriques est tomb�. La petite Sophie se r�veille de son cauchemar.", SoundManager.Instance.lostSound);
+                EndGame("Partie perdue! Un des défenseurs des rêves est tombé. La petite Sophie-Rose se réveille.", SoundManager.Instance.lostSound);
                 break;
             default:
                 break;
@@ -50,7 +50,7 @@ public class WinLoseHandler : Singleton<WinLoseHandler>
         GameData.Instance.UpdateAllTowersData();
         var turretBuiltCount = GameData.Instance.turrets.FindAll(t => t.isBuilt).Count;
 
-        endGameText.text = text + $" Vous avez construit {turretBuiltCount} tours. Vous n'aurez pas acc�s � ces emplacements de construction pour les prochaines parties";
+        endGameText.text = text + $"\n Vous avez construit {turretBuiltCount} tours. Elles seront détruites la prochaine partie";
         AudioSource.PlayClipAtPoint(oneShotClip, transform.position);
         endGameCanvas.SetActive(true);  
     }
